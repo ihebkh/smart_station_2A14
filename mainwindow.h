5ac53a9367424691af53ca5a303e9908f320fc18
ismailgharnougui <72952720+ismailgharnougui@users.noreply.github.com>
2021-12-09 11:25:18 +0100

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -1,82 +1,39 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include "arduino.h"
 #include <QMainWindow>
-#include"reparations.h"
-#include <QDialog>
 
-#include <QPixmap>
-#include <QFile>
-#include <QDialog>
-#include <QFileDialog>
-
-#include <QUrl>
-#include <QSystemTrayIcon>
-#include<QPropertyAnimation>
-#include <QRegularExpression>
-#include <QTranslator>
-#include <QPixmap>
-
-#include <QtNetwork/QAbstractSocket>
-#include <QtNetwork/QSslSocket>
-#include<QFileDialog>
-#include<QTextDocument>
-#include<QTextStream>
-#include<QGraphicsView>
-#include<QtPrintSupport/QPrintDialog>
-#include<QPdfWriter>
-#include<QSqlQueryModel>
-#include<QSqlQuery>
-#include<QSystemTrayIcon>
-#include<QUrlQuery>
-#include<QJsonDocument>
-#include<QJsonObject>
-#include<QJsonArray>
-#include <QDate>
-#include <QTime>
-#include<QMessageBox>
-#include<QSqlTableModel>
-#include<QItemSelectionModel>
-#include<QTableWidgetItem>
-#include <QtPrintSupport/QPrinter>
-#include <QDesktopServices>
-#include <QDesktopWidget>
-#include <QCoreApplication>
-#include "smtp.h"
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
-    void on_pb_ajouter_clicked();
-    void on_date_dispo_modifier_clicked();
-    void on_pb_supprimer_clicked();
 
-    void on_rechercher_button_clicked();
-    void on_radioButton_croissant_clicked();
-    void on_radioButton_dcroissant_clicked();
-    void on_statsmed_clicked();
-    void on_pushButton_browse_email_clicked();
+    void update_label();   // slot permettant la mise à jour du label état de la lampe 1,
+    // ce slot est lancé à chaque réception d'un message de Arduino
 
-    void on_pushButton_send_email_clicked();
+    void on_pushButton_clicked();   // bouton ON
 
-    void on_btn_calculer_clicked();
-    void on_btn_imp_clicked();
+    void on_pushButton_2_clicked(); // bouton OFF
 
+    void on_pushButton_3_clicked(); // bouton +
 
-    void on_pb_exit_clicked();
+    void on_pushButton_4_clicked(); // bouton -
 
 private:
     Ui::MainWindow *ui;
-    Reparations R;
-    QStringList files;
+
+    QByteArray data; // variable contenant les données reçues
+
+    Arduino A; // objet temporaire
 };
+
 #endif // MAINWINDOW_H
